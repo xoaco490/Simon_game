2afb6994f722e03f27add933ea682a7fa44a2117
Xoa Cox <joakkoa@gmail.com>
2020-08-31 19:41:34 -0300

    proyect files added

---
+++ game.js
@@ -0,0 +1,30 @@
+var buttonColors = ["red","blue","green","yellow"];
+var gamePattern = [];
+var userClickedPattern = []
+
+$(".btn").click(function(){
+    var userChosenColor = $(this).attr('id');
+    userClickedPattern.push(userChosenColor);
+    playSound(userChosenColor);
+    animatePress(userChosenColor);
+});
+
+function nextSequence(){
+    let randomNumber = Math.floor(Math.random()*4);
+    let randomChosenColor = buttonColors[randomNumber];
+    gamePattern.push(randomChosenColor);
+    $("#"+randomChosenColor).fadeIn(100).fadeOut(100).fadeIn(100);
+    playSound(randomChosenColor);
+}
+
+function playSound(name){
+    let audio = new Audio("sounds/"+name+".mp3");
+    audio.play();
+}
+
+function animatePress(currentColor){
+    $("#"+currentColor).addClass("pressed");
+    setTimeout(function(){
+        $("#"+currentColor).removeClass("pressed");
+    }, 100)
+}
